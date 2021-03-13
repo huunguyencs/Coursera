92c3ae3ac55baa8fa62d02ce05025698278a3911
Huu Nguyen <thieuvan0405@gmail.com>
2021-03-13 20:39:45 +0700

    Fix assignment

--- Programming with a Purpose/hello/GreatCircle.java
+++ Programming with a Purpose/hello/GreatCircle.java
@@ -2,16 +2,20 @@
 
 public class GreatCircle {
     public static void main(String[] args) {
-        double x1 = Double.parseDouble(args[0]);
-        double y1 = Double.parseDouble(args[1]);
-        double x2 = Double.parseDouble(args[2]);
-        double y2 = Double.parseDouble(args[3]);
+        double x1 = Math.toRadians(Double.parseDouble(args[0]));
+        double y1 = Math.toRadians(Double.parseDouble(args[1]));
+        double x2 = Math.toRadians(Double.parseDouble(args[2]));
+        double y2 = Math.toRadians(Double.parseDouble(args[3]));
         double r = 6371.0;
 
-        double distance = 2 * r
-                * Math.asin(
-                        Math.sqrt(Math.pow(Math.sin(Math.toRadians((x2 - x1) / 2)), 2) + Math.cos(Math.toRadians(x1))
-                                * Math.cos(Math.toRadians(x2)) * Math.pow(Math.sin(Math.toRadians((y2 - y1) / 2)), 2)));
+
+        double distance = Math.sin((y2-y1)/2);
+        distance = Math.pow(distance, 2);
+        distance = Math.cos(x1)*Math.cos(x2)*distance;
+        distance = Math.pow(Math.sin((x2-x1)/2),2) + distance;
+        distance = Math.sqrt(distance);
+        distance = Math.asin(distance);
+        distance = 2*r*distance;
         System.out.println(distance + " kilometers");
     }
 }
