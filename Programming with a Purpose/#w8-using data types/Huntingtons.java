41ffb85ba662bed3a772ac799aa554d38d2e0549
Huu Nguyen <thieuvan0405@gmail.com>
2021-04-16 21:30:37 +0700

    Solved Huntingtons

--- Programming with a Purpose/#w8-using data types/Huntingtons.java
+++ Programming with a Purpose/#w8-using data types/Huntingtons.java
@@ -3,23 +3,60 @@ public class Huntingtons {
 
     // Returns the maximum number of consecutive repeats of CAG in the DNA string.
     public static int maxRepeats(String dna) {
-        return 0;
+        int len = dna.length();
+        int maxRepeats = 0;
+        int currIndex = dna.indexOf("CAG", 0);
+        while (currIndex != -1) {
+            int count = 0;
+            int curr = currIndex + 3;
+            boolean isExit = false;
+            while (curr < len - 3 && !isExit) {
+                count += 1;
+                if (!dna.substring(curr, curr + 3).equals("CAG")) {
+                    isExit = true;
+                }
+                curr += 3;
+            }
+
+            maxRepeats = maxRepeats < count ? count : maxRepeats;
+            currIndex = dna.indexOf("CAG", curr);
+        }
+        return maxRepeats;
     }
 
     // Returns a copy of s, with all whitespace (spaces, tabs, and newlines)
     // removed.
     public static String removeWhitespace(String s) {
-        return "";
+        String res = s;
+        res = res.replaceAll(" ", "");
+        res = res.replaceAll("\t", "");
+        res = res.replaceAll("\n", "");
+        return res;
     }
 
     // Returns one of these dianoses corresponding to the maximum number of repeats:
     // "not human", "normal", "high risk" of "Huntington's".
     public static String diagnose(int maxRepeats) {
-        return "";
+        if (maxRepeats < 10) {
+            return "not human";
+        } else if (maxRepeats < 36) {
+            return "normal";
+        } else if (maxRepeats < 40) {
+            return "high risk";
+        } else if (maxRepeats < 181) {
+            return "Huntington's";
+        } else {
+            return "not human";
+        }
     }
 
     // Sample client (see below).
     public static void main(String[] args) {
-
+        In in = new In(args[0]);
+        String dna = in.readAll();
+        dna = removeWhitespace(dna);
+        int maxRepeats = maxRepeats(dna);
+        System.out.println("max repeats = " + maxRepeats);
+        System.out.println(diagnose(maxRepeats));
     }
 }
--- Programming with a Purpose/#w8-using data types/KernelFilter.java
+++ Programming with a Purpose/#w8-using data types/KernelFilter.java
@@ -2,32 +2,41 @@
 public class KernelFilter {
     // Returns a new picture that applies the identity filter to the given picture.
     public static Picture identity(Picture picture) {
+        double[][] kernel = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
 
     }
 
     // Returns a new picture that applies a Gaussian blur filter to the given
     // picture.
     public static Picture gaussian(Picture picture) {
+        double[][] kernel = { { 1 / 16, 1 / 8, 1 / 16 }, { 1 / 8, 1 / 4, 1 / 8 }, { 1 / 16, 1 / 8, 1 / 16 } };
 
     }
 
     // Returns a new picture that applies a sharpen filter to the given picture.
     public static Picture sharpen(Picture picture) {
+        double[][] kernel = { { 0, -1, 0 }, { -1, 5, -1 }, { 0, -1, 0 } };
 
     }
 
     // Returns a new picture that applies an Laplacian filter to the given picture.
     public static Picture laplacian(Picture picture) {
+        double[][] kernel = { { -1, -1, -1 }, { -1, 8, -1 }, { -1, -1, -1 } };
 
     }
 
     // Returns a new picture that applies an emboss filter to the given picture.
     public static Picture emboss(Picture picture) {
+        double[][] kernel = { { -2, -1, 0 }, { -1, 1, 1 }, { 0, 1, 2 } };
 
     }
 
     // Returns a new picture that applies a motion blur filter to the given picture.
     public static Picture motionBlur(Picture picture) {
+        double[][] kernel = new double[9][9];
+        for (int i = 0; i < 9; i++) {
+            kernel[i][i] = 1 / 9;
+        }
 
     }
 
