12c5af54f8563d5ba2a2578f5abf92da578b4ecc
Huu Nguyen <thieuvan0405@gmail.com>
2021-04-19 13:44:43 +0700

    Complete week 9

--- Programming with a Purpose/#w9-creating-data-types/Clock.java
+++ Programming with a Purpose/#w9-creating-data-types/Clock.java
@@ -1,38 +1,91 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Clock {
+
+    private static final int HOUR_PER_DAY = 24;
+    private static final int MINUTE_PER_HOUR = 60;
+
+    private int hour;
+    private int minute;
 
     // Creates a clock whose initial time is h hours and m minutes.
     public Clock(int h, int m) {
-
+        if (h < 0 || h >= HOUR_PER_DAY || m < 0 || m >= MINUTE_PER_HOUR) {
+            throw new IllegalArgumentException();
+        }
+        hour = h;
+        minute = m;
     }
 
     // Creates a clock whose initial time is specified as a string, using the format
     // HH:MM
     public Clock(String s) {
-
+        Pattern pattern = Pattern.compile("^[0-9]{2}:[0-9]{2}$");
+        Matcher matcher = pattern.matcher(s);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException();
+        }
+        int h = Integer.parseInt(s.substring(0, 2));
+        int m = Integer.parseInt(s.substring(3, 5));
+        if (h < 0 || h >= HOUR_PER_DAY || m < 0 || m >= MINUTE_PER_HOUR) {
+            throw new IllegalArgumentException();
+        }
+        hour = h;
+        minute = m;
     }
 
     // Returns a string representation of this clock, using the format HH:MM
     public String toString() {
-        return "";
+        return String.format("%02d:%02d", hour, minute);
     }
 
     // Is the time on this clock earlier than the time on that one?
     public boolean isEarlierThan(Clock that) {
-        return true;
+        if (hour < that.hour) {
+            return true;
+        } else if (hour > that.hour) {
+            return false;
+        } else if (minute < that.minute) {
+            return true;
+        }
+        return false;
     }
 
     // Adds 1 minute to the time on this clock.
     public void tic() {
-
+        toc(1);
     }
 
     // Adds Δ minutes to the time on this clock.
     public void toc(int delta) {
-
+        if (delta < 0) {
+            throw new IllegalArgumentException();
+        }
+        minute += delta;
+        if (minute >= MINUTE_PER_HOUR) {
+            int h = minute / MINUTE_PER_HOUR;
+            hour += h;
+            minute -= MINUTE_PER_HOUR * h;
+            if (hour >= HOUR_PER_DAY) {
+                hour %= HOUR_PER_DAY;
+            }
+        }
     }
 
     // Test client (see below).
     public static void main(String[] args) {
+        int h = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]);
+        String s = args[2];
+        Clock clock1 = new Clock(h, m);
+        Clock clock2 = new Clock(s);
+        clock1.tic();
+        clock2.toc(2);
+        StdOut.println("Clock 1: " + clock1);
+        StdOut.println("Clock 2: " + clock2);
+        StdOut.println(
+                clock1.isEarlierThan(clock2) ? "Clock 1 is earlier than clock 2" : "Clock 2 is earlier than clock 1");
 
     }
 }
--- Programming with a Purpose/#w9-creating-data-types/ColorHSB.java
+++ Programming with a Purpose/#w9-creating-data-types/ColorHSB.java
@@ -1,28 +1,58 @@
 
 public class ColorHSB {
 
+    private final int hue;
+    private final int saturation;
+    private final int brightness;
+
     // Creates a color with hue h, saturation s, and brightness b.
     public ColorHSB(int h, int s, int b) {
-
+        hue = h;
+        saturation = s;
+        brightness = b;
     }
 
     // Returns a string representation of this color, using the format (h, s, b).
     public String toString() {
-        return "";
+        return "(" + hue + ", " + saturation + ", " + brightness + ")";
     }
 
     // Is this color a shade of gray?
     public boolean isGrayscale() {
-        return true;
+        return saturation == 0 || brightness == 0;
     }
 
     // Returns the squared distance between the two colors.
     public int distanceSquaredTo(ColorHSB that) {
-        return 0;
+        int a = (int) Math.min(Math.pow(hue - that.hue, 2), Math.pow(360 - Math.abs(hue - that.hue), 2));
+        int b = (int) Math.pow(saturation - that.saturation, 2);
+        int c = (int) Math.pow(brightness - that.brightness, 2);
+        return a + b + c;
     }
 
     // Sample client (see below).
     public static void main(String[] args) {
-
+        int h = Integer.parseInt(args[0]);
+        int s = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[2]);
+        ColorHSB color = new ColorHSB(h, s, b);
+        ColorHSB minDis = new ColorHSB(0, 0, 0);
+        String name = "";
+        int distance = Integer.MAX_VALUE;
+        while (StdIn.hasNextLine()) {
+            String n = StdIn.readString();
+            int hTmp = StdIn.readInt();
+            int sTmp = StdIn.readInt();
+            int bTmp = StdIn.readInt();
+            ColorHSB tmp = new ColorHSB(hTmp, sTmp, bTmp);
+            int dis = color.distanceSquaredTo(tmp);
+            if (dis < distance) {
+                distance = dis;
+                minDis = tmp;
+                name = n;
+            }
+            StdIn.readLine();
+        }
+        StdOut.println(name + " " + minDis.toString());
     }
 }
